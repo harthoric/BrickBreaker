16e8039b996df0364cf8256590c8271596674300
harthoric <harthoric@gmail.com>
2019-06-02 17:00:07 +0100

    Add constructor

--- entity/Paddle.java
+++ entity/Paddle.java
@@ -1,5 +1,31 @@
 package com.harthoric.brickbreaker.entity;
 
-public class Paddle {
+public class Paddle extends Vector2D {
+
+  public Rectangle player;
+
+	public Paddle(int x, int y, Rectangle player) {
+		super(x, y);
+		this.player = player;
+	}
+
+	public void moveUp(int moveBy) {
+		super.setY(super.getY() + moveBy);
+	}
+
+	public void moveDown(int moveBy) {
+		super.setY(super.getY() - moveBy);
+	}
+
+	public void moveLeft(int moveBy) {
+		super.setX(super.getX() + moveBy);
+		player.setTranslateX(-getX());
+
+	}
+
+	public void moveRight(int moveBy) {
+		super.setX(super.getX() - moveBy);
+		player.setTranslateX(-getX());
+	}
 
 }
