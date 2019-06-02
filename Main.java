20e4deea7d5a3ae426a7e18fee173e62a13f3541
harthoric <harthoric@gmail.com>
2019-06-02 17:01:58 +0100

    Add game properties

--- Main.java
+++ Main.java
@@ -5,13 +5,29 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  public static final int WIDTH = 600, HEIGHT = 400;
+	public static final Color colours[] = new Color[] { Color.DARKGRAY, Color.DIMGRAY, Color.GRAY, Color.WHITE, Color.DARKRED,
+			Color.DARKGOLDENROD };
+	public static HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+
+	private Canvas canvas;
+	private StackPane root;
+	private Scene scene;
+
+	private BrickLoader brickLoader;
+	private static Player player;
+	private static Ball ball;
+
+	Text lifeCounter;
+	Rectangle leftBorder, rightBorder;
+
   public static void main(String args[]) {
     launch(args);
   }
 
   @Override
   public void start(Stage primaryStage) {
-    
+
   }
 
 }
