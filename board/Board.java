7373a8a80a1aa0b15eb0ff744f7fa56887d16c82
harthoric <harthoric@gmail.com>
2019-06-02 17:21:24 +0100

    Initialise game properties; setup board and board mechanics

--- board/Board.java
+++ board/Board.java
@@ -19,7 +19,19 @@ public class Board {
 	Rectangle leftBorder, rightBorder;
 
   public Board() {
-    
+    canvas = new Canvas(WIDTH, HEIGHT);
+		canvas.setFocusTraversable(true);
+		root = new StackPane();
+		root.setStyle("-fx-background-color: black");
+		player = new Player(0, HEIGHT / 2 - 20, 5);
+		ball = new Ball(1.005f, new Vector2D(-2.0f * 1.22f, 2.35f * 1.22f), new Vector2D(0, HEIGHT / 2 - 50), new Circle(5, Color.DARKRED));
+		brickLoader = new BrickLoader(5, 10, 40, 40, false);
+		setupBorders();
+		setupScore();
+		addRootChild(canvas, brickLoader.getBrickGroup(), ball.ball, player.player, lifeCounter, leftBorder, rightBorder);
+		scene = new Scene(root);
+		scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+		scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
   }
 
 }
