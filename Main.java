09204522ca2fbb184149c5672f8db9d948374385
harthoric <harthoric@gmail.com>
2019-06-02 17:02:44 +0100

    Update properties to Board class

--- Main.java
+++ Main.java
@@ -5,21 +5,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-  public static final int WIDTH = 600, HEIGHT = 400;
-	public static final Color colours[] = new Color[] { Color.DARKGRAY, Color.DIMGRAY, Color.GRAY, Color.WHITE, Color.DARKRED,
-			Color.DARKGOLDENROD };
-	public static HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
-
-	private Canvas canvas;
-	private StackPane root;
-	private Scene scene;
-
-	private BrickLoader brickLoader;
-	private static Player player;
-	private static Ball ball;
-
-	Text lifeCounter;
-	Rectangle leftBorder, rightBorder;
+  public static Board board;
+	private KeyHandler keyHandler;
 
   public static void main(String args[]) {
     launch(args);
