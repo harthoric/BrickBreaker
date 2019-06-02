b6146b37d1dfd005bd1813ed9ec640176a0fb18a
harthoric <harthoric@gmail.com>
2019-06-02 17:01:14 +0100

    Add ball physics

--- entity/Ball.java
+++ entity/Ball.java
@@ -15,4 +15,50 @@ public class Ball {
 		this.ball = ball;
 	}
 
+  public Vector2D getVelocity() {
+		return velocity;
+	}
+
+	public Vector2D getPosition() {
+		return position;
+	}
+
+	public void setVelocity(Vector2D velocity) {
+		this.velocity = velocity;
+	}
+
+	public void setPosition(Vector2D position) {
+		this.position = position;
+	}
+
+	public void updatePos() {
+		position.setX(position.getX() + velocity.getX());
+		position.setY(position.getY() + velocity.getY());
+	}
+
+	public void increaseVel() {
+		velocity.setY(velocity.getY() * ACCELERATION);
+		velocity.setX(velocity.getX() * ACCELERATION);
+	}
+
+	public void changeXVel() {
+		velocity.setX(-velocity.getX());
+	}
+
+	public void changeYVel() {
+		velocity.setY(-velocity.getY());
+	}
+
+	public void explode() {
+		ball.setFill(Color.WHITE);
+		ball.setScaleX(2);
+		ball.setScaleY(2);
+	}
+
+	public void resetExplode() {
+		ball.setFill(Color.DARKRED);
+		ball.setScaleX(1);
+		ball.setScaleY(1);
+	}
+
 }
