200227e420cdf6dbd5ab71c1ff44ed00147a537b
harthoric <harthoric@gmail.com>
2019-06-02 17:00:37 +0100

    Add constructor

--- entity/Ball.java
+++ entity/Ball.java
@@ -3,7 +3,16 @@ package com.harthoric.brickbreaker.entity;
 public class Ball {
 
   // Amount the ball increases each time it hits a brick
-  private final float ACCELERATION;
-  private Vector2D velocity, position;
+	private final float ACCELERATION;
+	private Vector2D velocity, position;
+
+	public Circle ball;
+
+	public Ball(final float ACCELERATION, Vector2D velocity, Vector2D position, Circle ball) {
+		this.ACCELERATION = ACCELERATION;
+		this.velocity = velocity;
+		this.position = position;
+		this.ball = ball;
+	}
 
 }
